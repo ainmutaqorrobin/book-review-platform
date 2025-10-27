a97b2b58493887eec9d5278c57fa9abe99755dad
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-10-27 19:47:28 +0800

    refactor (backend): split app for testing purpose

---
+++ backend/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { json } from "body-parser";
+import { BookRouter } from "./routes/book";
+import { errorHandler } from "./middlewares/errorHandler";
+import { AppError } from "./utils/AppError";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "📚 Book Review API is running!!" });
+});
+
+app.use("/books", BookRouter);
+
+app.all(/.*/, async (req, res, next) => {
+  throw new AppError(`Route ${req.originalUrl} not found`, 404);
+});
+
+app.use(errorHandler);
+
+export { app };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,31 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { json } from "body-parser";
-import { BookRouter } from "./routes/book";
-import { errorHandler } from "./middlewares/errorHandler";
-import { AppError } from "./utils/AppError";
+import { app } from "./app";
 
-dotenv.config();
-
-const app = express();
 const PORT = process.env.PORT || 4000;
-
-app.use(cors());
-app.use(json());
-
-app.get("/", (req, res) => {
-  res.json({ message: "📚 Book Review API is running!!" });
-});
-
-app.use("/books", BookRouter);
-
-app.all(/.*/, async (req, res, next) => {
-  throw new AppError(`Route ${req.originalUrl} not found`, 404);
-});
-
-app.use(errorHandler);
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+);
