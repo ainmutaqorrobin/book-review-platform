a42a8cfc96579bad429e11f6df1a854804ff873f
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-11-08 19:33:26 +0800

    infra (backend): add users sql comand

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,3 +1,14 @@
+-- users table
+CREATE TABLE
+    users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        role VARCHAR(50) NOT NULL DEFAULT 'user', -- e.g. 'user', 'admin'
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
 -- books table
 CREATE TABLE
     IF NOT EXISTS books (
